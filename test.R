4d0c90954a8f737dc87293760ac7ed5c5050c8d2
Alison Kingman <alisonkingman@gmail.com>
2015-12-02 20:41:13 -0800

    added test script

---
+++ test.R
@@ -0,0 +1,37 @@
+require(Formula)
+require(partykit)
+require(lubridate)
+require(dplyr)
+
+data= read.csv("train.csv")
+
+#take out hour, month and year
+data = data %>% 
+  mutate(Time = ymd_hms(Dates)) %>%
+  mutate(hour = as.factor(hour(Time)), 
+         month = as.factor(month(Time)), 
+         year=year(Time))
+
+#remove description, resolution adress, Date, Time
+data = data[,-c(1,3,6,7,10)]
+
+#only data from 2006 and before
+data = data %>%
+  filter(year >2005)
+data$year = as.factor(data$year)
+
+
+
+train= data[seq(1:500),]
+test = data[seq(501:1000),]
+t = ctree(Category~., data = train)
+predict(t, test)
+plot(t)
+
+
+# use only data from 2006 and on
+#remove description, resolution adress
+#time of day: categorical by hour
+#month
+#year
+#keep district, dayofweek, x, y as is
